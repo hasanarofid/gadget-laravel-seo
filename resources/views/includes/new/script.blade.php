e49bfb89b90b315a1da7aab61b2c313811133150
hasanarofid <hasanarofid@gmail.com>
2024-07-26 14:29:51 +0700

    perbaikan

--- resources/views/includes/new/script.blade.php
+++ resources/views/includes/new/script.blade.php
@@ -1,4 +1,19 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    
+    <script>
+//         document.addEventListener('DOMContentLoaded', function () {
+//     const submenuItems = document.querySelectorAll('.has-submenu');
+
+//     submenuItems.forEach(item => {
+//         item.addEventListener('mouseenter', function () {
+//             document.body.classList.add('blur');
+//         });
+
+//         item.addEventListener('mouseleave', function () {
+//             document.body.classList.remove('blur');
+//         });
+//     });
+// });
+
+    </script>
