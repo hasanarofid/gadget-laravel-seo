034c95fd8348bc034146f16b075482cbdc04e179
hasanarofid <hasanarofid@gmail.com>
2024-07-28 13:52:28 +0700

    master

---
+++ resources/views/includes/halamandepan/slider1.blade.php
@@ -0,0 +1,178 @@
+<section class="homepage-section standalone-module" data-module-template="tv-plus-gallery" data-analytics-region="tv-plus-gallery">
+    <div class="module-content">
+      <script type="text/javascript">
+        window.tvPlusHpData = {
+          "data": {
+            "shelf": {
+              "cachingPolicy": {
+                "cache": true,
+                "maxAge": 0,
+                "type": "RefreshWithCanvas"
+              },
+              "displayType": "river",
+              "id": "edt.col.626ae340-a5f3-4474-8cc5-8b716ded72e5",
+              "items": [
+                {
+                  "extendedMetadata": {
+                    "images": {
+                      "channelSplashWide": {
+                        "width": 4320,
+                        "height": 1800,
+                        "url": "https://is1-ssl.mzstatic.com/image/thumb/Features/v4/e4/5b/70/e45b7075-92f2-feb7-39da-81a359533853/2b9847c6-1def-422c-9fff-5538a7fcf2bc.png/{w}x{h}sr.{f}",
+                        "joeColor": "b:rgb(9,9,9) p:rgb(218,219,233) s:rgb(220,183,163) t:rgb(176,177,188) q:rgb(178,148,132)"
+                      },
+                      "posterArt": {
+                        "width": 3840,
+                        "height": 2160,
+                        "url": "https://is1-ssl.mzstatic.com/image/thumb/p-dFQhrrenz0eg8_smgu9w/{w}x{h}.{f}",
+                        "supportsLayeredImage": true,
+                        "joeColor": "b:rgb(9,9,9) p:rgb(236,218,208) s:rgb(221,193,180) t:rgb(190,176,168) q:rgb(179,156,145)"
+                      },
+                      "channelSplashTall": {
+                        "width": 1680,
+                        "height": 3636,
+                        "url": "https://is1-ssl.mzstatic.com/image/thumb/Features/v4/86/4a/f2/864af2c0-8668-eb2b-62f8-42c649aad697/7cbd8806-e701-4183-84e7-f7c2c61f9b08.png/{w}x{h}tc.{f}",
+                        "joeColor": "b:rgb(5,4,4) p:rgb(221,220,232) s:rgb(201,173,157) t:rgb(178,177,186) q:rgb(162,139,126)"
+                      },
+                      "singleColorContentLogo": {
+                        "width": 3502,
+                        "height": 1258,
+                        "url": "https://is1-ssl.mzstatic.com/image/thumb/c5xAg8ALgLjQtvpsVgTg7g/{w}x{h}.{f}",
+                        "joeColor": "b:rgb(255,255,255) p:rgb(12,12,12) s:rgb(25,25,25) t:rgb(61,61,61) q:rgb(71,71,71)"
+                      }
+                    },
+                    "longNote": "Presume nothing.",
+                    "shortNote": "Jake Gyllenhaal is an attorney whose life unravels when his colleague is murdered."
+                  },
+                  "genres": [
+                    {
+                      "id": "umc.gnr.tv.crime",
+                      "name": "Crime",
+                      "type": "Genre",
+                      "url": "https://tv.apple.com/us/genre/crime/umc.gnr.tv.crime"
+                    }
+                  ],
+                  "id": "umc.cmc.5hnqrhwtzt3esr7rb1wq2ppvn",
+                  "images": {
+                    "shelfImage": {
+                      "height": 2160,
+                      "isP3": false,
+                      "joeColor": "b:rgb(9,9,9) p:rgb(236,218,208) s:rgb(221,193,180) t:rgb(190,176,168) q:rgb(179,156,145)",
+                      "supportsLayeredImage": true,
+                      "url": "https://is1-ssl.mzstatic.com/image/thumb/p-dFQhrrenz0eg8_smgu9w/{w}x{h}.{f}",
+                      "width": 3840
+                    },
+                    "shelfImageBackground": {
+                      "height": 2160,
+                      "isP3": false,
+                      "joeColor": "b:rgb(9,9,9) p:rgb(224,195,182) s:rgb(214,169,143) t:rgb(181,158,147) q:rgb(173,137,116)",
+                      "supportsLayeredImage": false,
+                      "url": "https://is1-ssl.mzstatic.com/image/thumb/Bmd-_E0WDnmV_fKE3kIUwQ/{w}x{h}.{f}",
+                      "width": 3840
+                    },
+                    "transitionImage": {
+                      "height": 3240,
+                      "isP3": false,
+                      "isUber": true,
+                      "joeColor": "b:rgb(0,0,6) p:rgb(216,152,127) s:rgb(236,86,53) t:rgb(173,121,103) q:rgb(189,68,43)",
+                      "supportsLayeredImage": false,
+                      "url": "https://is1-ssl.mzstatic.com/image/thumb/bYeZOr1ocJJzVk9CdQbH0Q/{w}x{h}sr.{f}",
+                      "width": 4320
+                    }
+                  },
+                  "isAppleOriginal": true,
+                  "releaseDate": 1718150400000,
+                  "secondaryActions": ["AddToUpNext"],
+                  "title": "Presumed Innocent",
+                  "type": "Show",
+                  "url": "https://tv.apple.com/us/show/presumed-innocent/umc.cmc.5hnqrhwtzt3esr7rb1wq2ppvn"
+                },
+                {
+                  "extendedMetadata": {
+                    "caption": "NEW EPISODES FRIDAYS",
+                    "images": {
+                      "channelSplashWide": {
+                        "width": 4320,
+                        "height": 1800,
+                        "url": "https://is1-ssl.mzstatic.com/image/thumb/Features/v4/6e/cd/5b/6ecd5b17-cd76-c312-79ee-e26a71b6c6d7/d8074670-fb1d-4107-8e76-8ce35e6e81d8.png/{w}x{h}sr.{f}",
+                        "joeColor": "b:rgb(25,40,46) p:rgb(216,220,178) s:rgb(190,195,171) t:rgb(178,184,151) q:rgb(157,164,146)"
+                      },
+                      "posterArt": {
+                        "width": 3840,
+                        "height": 2160,
+                        "url": "https://is1-ssl.mzstatic.com/image/thumb/SCG2BRdS-gXePqsduWlMmQ/{w}x{h}.{f}",
+                        "supportsLayeredImage": true,
+                        "joeColor": "b:rgb(36,56,64) p:rgb(213,217,175) s:rgb(202,206,171) t:rgb(178,185,153) q:rgb(169,176,150)"
+                      },
+                      "channelSplashTall": {
+                        "width": 1680,
+                        "height": 3636,
+                        "url": "https://is1-ssl.mzstatic.com/image/thumb/Features/v4/02/55/87/02558796-3d05-7c41-2919-e8d3b6842e62/cce37545-c528-4688-9201-d1b30a10f64a.png/{w}x{h}tc.{f}",
+                        "joeColor": "b:rgb(24,35,31) p:rgb(213,216,176) s:rgb(205,204,173) t:rgb(175,180,147) q:rgb(169,170,145)"
+                      },
+                      "singleColorContentLogo": {
+                        "width": 7714,
+                        "height": 806,
+                        "url": "https://is1-ssl.mzstatic.com/image/thumb/RLug43qIeK252EPlNkMD_w/{w}x{h}.{f}",
+                        "joeColor": "b:rgb(255,255,255) p:rgb(12,12,12) s:rgb(25,25,25) t:rgb(61,61,61) q:rgb(71,71,71)"
+                      }
+                    },
+                    "longNote": "Natalie Portman and Moses Ingram star in this compelling drama.",
+                    "shortNote": "Natalie Portman stars in this dramatic retelling of real-life events."
+                  },
+                  "genres": [
+                    {
+                      "id": "umc.gnr.tv.drama",
+                      "name": "Drama",
+                      "type": "Genre",
+                      "url": "https://tv.apple.com/us/genre/drama/umc.gnr.tv.drama"
+                    }
+                  ],
+                  "id": "umc.cmc.10g27sqps55bkhvgz9nsmhoj1",
+                  "images": {
+                    "shelfImage": {
+                      "height": 2160,
+                      "isP3": false,
+                      "joeColor": "b:rgb(36,56,64) p:rgb(213,217,175) s:rgb(202,206,171) t:rgb(178,185,153) q:rgb(169,176,150)",
+                      "supportsLayeredImage": true,
+                      "url": "https://is1-ssl.mzstatic.com/image/thumb/SCG2BRdS-gXePqsduWlMmQ/{w}x{h}.{f}",
+                      "width": 3840
+                    },
+                    "shelfImageBackground": {
+                      "height": 2160,
+                      "isP3": false,
+                      "joeColor": "b:rgb(36,56,64) p:rgb(213,217,175) s:rgb(202,206,171) t:rgb(178,185,153) q:rgb(169,176,150)",
+                      "supportsLayeredImage": false,
+                      "url": "https://is1-ssl.mzstatic.com/image/thumb/SCG2BRdS-gXePqsduWlMmQ/{w}x{h}.{f}",
+                      "width": 3840
+                    },
+                    "transitionImage": {
+                      "height": 4320,
+                      "isP3": false,
+                      "joeColor": "b:rgb(38,34,29) p:rgb(217,209,203) s:rgb(202,176,168) t:rgb(181,171,164) q:rgb(169,148,140)",
+                      "supportsLayeredImage": false,
+                      "url": "https://is1-ssl.mzstatic.com/image/thumb/Features/v4/6e/cd/5b/6ecd5b17-cd76-c312-79ee-e26a71b6c6d7/d8074670-fb1d-4107-8e76-8ce35e6e81d8.png/{w}x{h}sr.{f}",
+                      "width": 4320
+                    }
+                  },
+                  "isAppleOriginal": true,
+                  "releaseDate": 1718659200000,
+                  "secondaryActions": ["AddToUpNext"],
+                  "title": "Lady in the Lake",
+                  "type": "Show",
+                  "url": "https://tv.apple.com/us/show/lady-in-the-lake/umc.cmc.10g27sqps55bkhvgz9nsmhoj1"
+                }
+              ],
+              "itemsDisplayStyle": "single",
+              "itemsLayoutStyle": "grid",
+              "numItemsToDisplay": 1,
+              "title": "TV+ Series Gallery",
+              "type": "Gallery",
+              "url": "https://tv.apple.com/us/shelf/edt.col.626ae340-a5f3-4474-8cc5-8b716ded72e5"
+            }
+          }
+        };
+      </script>
+    </div>
+  </section>
+  
