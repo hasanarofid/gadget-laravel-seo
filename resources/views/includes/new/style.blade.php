82ffa7fe433c9a792edb034fabedb83f54a14b40
Andrean Ricky <andreansetyaji@gmail.com>
2024-07-28 10:59:55 +0700

    merubah logo di title website

--- resources/views/includes/new/style.blade.php
+++ resources/views/includes/new/style.blade.php
@@ -1,5 +1,5 @@
-<link rel="shortcut icon" href="{{ asset('theme/img/logoophone1.png') }}" type="image/x-icon">
-{{-- <img src="{{ asset('theme/img/logoophone1.png') }}" alt=""> --}}
+<link rel="shortcut icon" href="{{ asset('theme/img/logoophone2.png') }}" type="image/x-icon">
+{{-- <img src="{{ asset('theme/img/logoophone2.png') }}" alt=""> --}}
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="{{ asset('theme/style.css') }}">
 {{-- <link rel="stylesheet" href="{{ asset('theme/style2.css') }}"> --}}
