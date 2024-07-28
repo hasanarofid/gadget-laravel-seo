68c2f0ecbf658bf83976e202319778833492f06a
hasanarofid <hasanarofid@gmail.com>
2024-07-28 21:16:31 +0700

    setting database

---
+++ database/migrations/2024_07_28_140405_create_overviewtipe_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOverviewtipeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('overviewtipe', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('produk_id');
+            $table->integer('kategori_id');
+            $table->string('title');
+            $table->string('landing');
+            $table->text('diskipsi');
+            $table->foreign('produk_id')->references('id')->on('produk')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('overviewtipe');
+    }
+}
